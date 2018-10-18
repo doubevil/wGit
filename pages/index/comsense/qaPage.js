ec56a6a87ca18856680426d4fad03b53a1e550c2
winGit <amymos@126.com>
2018-10-18 17:15:39 +0800

    weixin app - qu&an sys

---
+++ pages/index/comsense/qaPage.js
@@ -0,0 +1,95 @@
+// pages/index/comsense/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    checkboxItems: [{
+        name: 'A',
+        value: 'A.美国'
+      },
+      {
+        name: 'B',
+        value: 'B.中国',
+        checked: 'true'
+      },
+      {
+        name: 'C',
+        value: 'C.巴西'
+      },
+      {
+        name: 'D',
+        value: 'D.日本'
+      },
+    ]
+  },
+  checkboxChange: function(e) {
+    var checked = e.detail.value
+    var changed = {}
+    for (var i = 0; i < this.data.checkboxItems.length; i++) {
+      if (checked.indexOf(this.data.checkboxItems[i].name) !== -1) {
+        changed['checkboxItems[' + i + '].checked'] = true
+      } else {
+        changed['checkboxItems[' + i + '].checked'] = false
+      }
+    }
+    this.setData(changed)
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
